49aad718df14d7282a8326398b54abec66e1a5a5
alekya-SQL <146048980+alekya-SQL@users.noreply.github.com>
2023-09-29 10:27:28 -0400

    Time: 5736 ms (25.49%), Space: 0B (100.00%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+
+
+select  s.product_id, first_year, s.quantity, s.price from
+(
+select  s.product_id,   min(year) as first_year from Sales s right join product p on p.product_id = s.product_id
+ where s.sale_id is not null
+group by s.product_id
+) a inner join Sales s on s.product_id = a.product_id and s.year = a.first_year
