02bed1b807235b5af733009eeb8e7e9b75682256
alekya-SQL <146048980+alekya-SQL@users.noreply.github.com>
2023-09-29 10:28:59 -0400

    Time: 5342 ms (73.76%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -3,7 +3,7 @@
 
 select  s.product_id, first_year, s.quantity, s.price from
 (
-select  s.product_id,   min(year) as first_year from Sales s right join product p on p.product_id = s.product_id
+select  s.product_id,   min(year) as first_year from Sales s inner join product p on p.product_id = s.product_id
  where s.sale_id is not null
 group by s.product_id
 ) a inner join Sales s on s.product_id = a.product_id and s.year = a.first_year
