7a910cd639489675bcbfeffacbe576eb1682d0c7
alekya <146048980+alekya-SQL@users.noreply.github.com>
2023-10-03 10:47:24 -0400

    Time: 562 ms (63.48%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,6 +1,5 @@
 /* Write your T-SQL query statement below */
-select * from
-(
+
 select id ,movie,description,rating from Cinema
 where description !='boring' and id%2 !=0
-) a order by rating desc
+order by rating desc
