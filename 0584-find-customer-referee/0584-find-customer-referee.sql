efb443870cbe79c27a37385d8fada82723674948
alekya <146048980+alekya-SQL@users.noreply.github.com>
2024-01-30 10:44:33 -0500

    Time: 1380 ms (32.18%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,4 @@
 /* Write your T-SQL query statement below */
-SELECT name FROM customer WHERE referee_id NOT LIKE '2' OR referee_id IS NULL;
---select  name from Customer  where referee_id is null or referee_id !=2
+
+select  name from Customer  where referee_id is null or referee_id !=2
 --isnull(referee_id,0) !=2
