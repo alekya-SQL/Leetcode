2b87bac099282ddcffcda6970de13801409e07b8
Alekya K <146048980+alekya-SQL@users.noreply.github.com>
2025-08-06 13:59:50 -0400

    Time: 302 ms (34.1%), Space: 67.1 MB (84.33%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.py
+++ 1258-article-views-i/1258-article-views-i.py
@@ -4,5 +4,5 @@ def article_views(views: pd.DataFrame) -> pd.DataFrame:
      return views[views.author_id == views.viewer_id
              ].sort_values('author_id'
              ).rename(columns = {'author_id': 'id'}
-             ).drop_duplicates('id').iloc[:,[1]]
+             ).drop_duplicates('id')[['id']]
     
